5bc0ed2b7c228caed4ef262c5922c946da744c8f
Aneto74 <aneto74@gmail.com>
2018-04-06 00:38:10 +0500

    v1.0

--- whatsyournumber.py
+++ whatsyournumber.py
@@ -4,7 +4,7 @@
 # Версия 1.3 — Теперь можно загадывать от 1 до 100 с проверкой
 import random
 
-max_number = 100
+max_number = 1000000000
 min_number = 1
 
 print('Тебе предоставляется возможность загадать число с',
@@ -22,11 +22,16 @@ counts = 0
 
 while comp_number != my_number:
     # 100 - 1 + 1 = 100 / 2 = 50
-    # 100 - 50 + 1 = 50 / 2 = 25
-    comp_number = int((max_number - min_number + 1) / 2)
-    # average_number_high = int(max_number)*0.66
-    # average_number_low = int(max_number)*0.33
-    # comp_number = random.randint(min_number, max_number)
+    # 49 - 1 + 1 = 49 / 2 = 24
+    # 49 - 25 + 25 = 23 / 2 = 
+
+    # 100 - 1 + 1 = 100 / 2 = 50
+    # 100 - 50 + 1 = 51 / 2 = 26
+    # 51 - 25
+    comp_number = int(((max_number - min_number + 1) / 2) + min_number)
+    # average_number_high = int((((max_number - min_number + 1) / 2) * 0.66) + min_number)
+    # average_number_low = int((((max_number - min_number + 1) / 2) * 0.33) + min_number)
+    # comp_number = random.choice([average_number_low, average_number_high])
     # comp_number = average_number
     print('Компьютер: «Может это число', comp_number, '?»')
     counts += 1
