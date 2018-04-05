833361f2ee9d0795474875fdf4c11d468e40503b
Aneto74 <aneto74@gmail.com>
2018-04-05 12:59:24 +0500

    new version

--- whatsyournumber.py
+++ whatsyournumber.py
@@ -4,25 +4,30 @@
 # Версия 1.3 — Теперь можно загадывать от 1 до 100 с проверкой
 import random
 
-print('Тебе предоставляется возможность загадать число с 1 до 100')
+max_number = 100
+min_number = 1
+
+print('Тебе предоставляется возможность загадать число с',
+      min_number, 'до', max_number)
 print('А я его отгадаю!')
 
 my_number = int(input('Загадай число:'))
 
-while True:
-	if my_number > 100 or my_number < 0:
-		print ('Загадать число нужно с 1 до 100 включительно')
-		my_number = int(input('Загадай число заново:'))
-	else:
-		break
+while (my_number > max_number or my_number < min_number):
+    print('Загадать число нужно с 1 до 100 включительно')
+    my_number = int(input('Загадай число заново:'))
 
 comp_number = ""
-max_number = 100
-min_number = 1
 counts = 0
 
 while comp_number != my_number:
-    comp_number = random.randint(min_number, max_number)
+    # 100 - 1 + 1 = 100 / 2 = 50
+    # 100 - 50 + 1 = 50 / 2 = 25
+    comp_number = int((max_number - min_number + 1) / 2)
+    # average_number_high = int(max_number)*0.66
+    # average_number_low = int(max_number)*0.33
+    # comp_number = random.randint(min_number, max_number)
+    # comp_number = average_number
     print('Компьютер: «Может это число', comp_number, '?»')
     counts += 1
     if comp_number < my_number:
